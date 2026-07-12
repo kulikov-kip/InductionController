8c781f0b119b4b1ad9a0f6e85cf2c20c575ba9e4
kulikov-kip <77kulikov@gmail.com>
2026-07-12 23:16:56 +0300

    Update encoder.h

--- encoder.h
+++ encoder.h
@@ -1,15 +1,1 @@
-#ifndef ENCODER_H
-#define ENCODER_H
 
-#include <Arduino.h>
-
-void encoderInit();
-void encoderTask();
-
-int8_t encoderGetDelta();
-
-bool encoderShortClick();
-
-bool encoderLongClick();
-
-#endif
