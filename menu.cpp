5cb1d68e37afd044fad8fc9ea5f933274a5018b7
kulikov-kip <77kulikov@gmail.com>
2026-07-12 23:06:36 +0300

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -49,3 +49,48 @@ void menuExit()
 {
     active=false;
 }
+static void drawMenu()
+{
+    displayClearBuffer();
+
+    uint8_t first=item;
+
+    if(first>4)
+        first=item-4;
+
+    for(uint8_t i=0;i<4;i++)
+    {
+        uint8_t index=first+i;
+
+        if(index>=MENU_ITEMS)
+            break;
+
+        char line[21];
+
+        if(index==item)
+            snprintf(line,21,">%s",menuItems[index]);
+        else
+            snprintf(line,21," %s",menuItems[index]);
+
+        switch(i)
+        {
+            case 0:
+                strncpy((char*)lcdBuffer[0],line,20);
+                break;
+
+            case 1:
+                strncpy((char*)lcdBuffer[1],line,20);
+                break;
+
+            case 2:
+                strncpy((char*)lcdBuffer[2],line,20);
+                break;
+
+            case 3:
+                strncpy((char*)lcdBuffer[3],line,20);
+                break;
+        }
+    }
+
+    displayUpdate();
+}
